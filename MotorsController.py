86c0601fc5838df944c3e9ac65490a6738750cb3
tokoroten-lab <tokoroten.lab@gmail.com>
2019-09-26 07:06:40 +0100

    add motors controller

---
+++ MotorsController.py
@@ -0,0 +1,78 @@
+from enum import Enum
+import RPi.GPIO as GPIO
+import time
+
+class MotorRotationDirection(Enum):
+    FORWARD = 0
+    BACKWARD = 1
+
+class Motor:
+    def __init__(self, forward_pin, backward_pin, pwm_freq):
+        self.FORWARD_PIN = forward_pin
+        self.BACKWARD_PIN = backward_pin
+        self.PWM_FREQ = pwm_freq
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup([self.FORWARD_PIN, self.BACKWARD_PIN], GPIO.OUT)
+        GPIO.output([self.FORWARD_PIN, self.BACKWARD_PIN], GPIO.LOW)
+        self.forward_pwm = GPIO.PWM(self.FORWARD_PIN, self.PWM_FREQ)
+        self.backward_pwm = GPIO.PWM(self.BACKWARD_PIN, self.PWM_FREQ)
+        self.forward_pwm.start(0)
+        self.backward_pwm.start(0)
+
+    def __del__(self):
+        print("Motor del")
+        self.forward_pwm.stop()
+        self.backward_pwm.stop()
+
+    def change_status(self, direction, speed):
+        if direction == MotorRotationDirection.FORWARD:
+            self.forward_pwm.ChangeDutyCycle(int(speed * 100))
+            self.backward_pwm.ChangeDutyCycle(0)
+        elif direction == MotorRotationDirection.BACKWARD:
+            self.forward_pwm.ChangeDutyCycle(0)
+            self.backward_pwm.ChangeDutyCycle(int(speed * 100))
+
+class MotorSelection(Enum):
+    LEFT_MOTOR = 0
+    RIGHT_MOTOR = 1
+
+class MotorsController:
+    def __init__(self, left_motor_forward_pin, left_motor_backward_pin, right_motor_forward_pin, right_motor_backward_pin, motors_pwm_freq):
+        self.left_motor = Motor(left_motor_forward_pin, left_motor_backward_pin, motors_pwm_freq)
+        self.right_motor = Motor(right_motor_forward_pin, right_motor_backward_pin, motors_pwm_freq)
+
+    def __del__(self):
+        print("MotorsController del")
+        GPIO.cleanup()
+
+    def change_motor_status(self, motor_selection, direction, speed):
+        if motor_selection == MotorSelection.LEFT_MOTOR:
+            self.left_motor.change_status(direction, speed)
+        elif motor_selection == MotorSelection.RIGHT_MOTOR:
+            self.right_motor.change_status(direction, speed)
+
+    def change_motors_status(self, direction, speed):
+        self.left_motor.change_status(direction, speed)
+        self.right_motor.change_status(direction, speed)
+
+def main():
+    motors_controller = MotorsController(21, 20, 23, 24, 1000)
+
+    motors_controller.change_motor_status(MotorSelection.LEFT_MOTOR, MotorRotationDirection.FORWARD, 0.0)
+    motors_controller.change_motor_status(MotorSelection.RIGHT_MOTOR, MotorRotationDirection.FORWARD, 1.0)
+    time.sleep(3)
+
+    motors_controller.change_motor_status(MotorSelection.LEFT_MOTOR, MotorRotationDirection.FORWARD, 0.0)
+    motors_controller.change_motor_status(MotorSelection.RIGHT_MOTOR, MotorRotationDirection.BACKWARD, 0.7)
+    time.sleep(3)
+
+    motors_controller.change_motor_status(MotorSelection.LEFT_MOTOR, MotorRotationDirection.FORWARD, 1.0)
+    motors_controller.change_motor_status(MotorSelection.RIGHT_MOTOR, MotorRotationDirection.FORWARD, 0.0)
+    time.sleep(3)
+
+    motors_controller.change_motor_status(MotorSelection.LEFT_MOTOR, MotorRotationDirection.BACKWARD, 0.7)
+    motors_controller.change_motor_status(MotorSelection.RIGHT_MOTOR, MotorRotationDirection.FORWARD, 0.0)
+    time.sleep(3)
+
+if __name__ == '__main__':
+    main()
