7bda5d959a4544b409a6b7bb14dedaa2fad5660b
tokoroten-lab <tokoroten.lab@gmail.com>
2019-09-29 19:57:58 +0900

    change constructors to safe and programs to zombies

--- GamepadController.py
+++ GamepadController.py
@@ -8,12 +8,12 @@ class GamepadController(threading.Thread):
     def __init__(self, left_motor_forward_pin, left_motor_backward_pin, right_motor_forward_pin, right_motor_backward_pin, motors_pwm_freq, server_ip, server_port):
         print("GamepadController __init__")
         self.SERVER_IP = server_ip
-        self.SERVER_PORT = server_port
-        self.LEFT_MOTOR_FORWARD_PIN = left_motor_forward_pin
-        self.LEFT_MOTOR_BACKWARD_PIN = left_motor_backward_pin
-        self.RIGHT_MOTOR_FORWARD_PIN = right_motor_forward_pin
-        self.RIGHT_MOTOR_BACKWARD_PIN = right_motor_backward_pin
-        self.MOTORS_PWM_FREQ = motors_pwm_freq
+        self.SERVER_PORT = int(server_port)
+        self.LEFT_MOTOR_FORWARD_PIN = int(left_motor_forward_pin)
+        self.LEFT_MOTOR_BACKWARD_PIN = int(left_motor_backward_pin)
+        self.RIGHT_MOTOR_FORWARD_PIN = int(right_motor_forward_pin)
+        self.RIGHT_MOTOR_BACKWARD_PIN = int(right_motor_backward_pin)
+        self.MOTORS_PWM_FREQ = int(motors_pwm_freq)
         threading.Thread.__init__(self)
 
     def __del__(self):
@@ -23,67 +23,73 @@ class GamepadController(threading.Thread):
         # ログ表示用インデント
         INDENT = '    '
 
-        # サーバーソケット生成
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
-            server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            server_sock.bind((self.SERVER_IP, self.SERVER_PORT))
-            server_sock.listen(1)
+        # モーター制御用
+        x_vector = [1, 1]
+        motors_controller = MotorsController.MotorsController(self.LEFT_MOTOR_FORWARD_PIN, self.LEFT_MOTOR_BACKWARD_PIN, self.RIGHT_MOTOR_FORWARD_PIN, self.RIGHT_MOTOR_BACKWARD_PIN, self.MOTORS_PWM_FREQ)
 
-            # サーバ情報表示
-            print('GamepadController Server {')
-            print(INDENT + 'IP   : {},'.format(self.SERVER_IP))
-            print(INDENT + 'PORT : {}'.format(self.SERVER_PORT))
-            print('}')
+        while(True):
+            try:
+                # サーバーソケット生成
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+                    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    server_sock.bind((self.SERVER_IP, self.SERVER_PORT))
+                    server_sock.listen(1)
 
-            # クライアント接続
-            client_sock, client_addr = server_sock.accept()
-            with client_sock:
-
-                # クライアント情報表示
-                print('GamepadController Client {')
-                print(INDENT + 'IP   : {},'.format(client_addr[0]))
-                print(INDENT + 'PORT : {}'.format(client_addr[1]))
-                print('}')
-
-                # モーター制御用
-                x_vector = [1, 1]
-                motors_controller = MotorsController.MotorsController(self.LEFT_MOTOR_FORWARD_PIN, self.LEFT_MOTOR_BACKWARD_PIN, self.RIGHT_MOTOR_FORWARD_PIN, self.RIGHT_MOTOR_BACKWARD_PIN, self.MOTORS_PWM_FREQ)
-
-                while True:
-                    # 操作情報取得
-                    receive_control = client_sock.recv(2048).decode('utf-8').split(' ')
-
-                    # 操作情報表示
-                    print('GamepadController Command {')
-                    print(INDENT + 'Type : {}'.format(receive_control[0]))
-                    print(INDENT + 'Value: {}'.format(receive_control[1]))
+                    # サーバ情報表示
+                    print('GamepadController Server {')
+                    print(INDENT + 'IP   : {},'.format(self.SERVER_IP))
+                    print(INDENT + 'PORT : {}'.format(self.SERVER_PORT))
                     print('}')
 
-                    # 複数のコマンドが連結されていた場合は操作を拒否
-                    if len(receive_control[1]) != receive_control[1].find(',') + 1:
-                        continue
+                    # クライアント接続
+                    client_sock, client_addr = server_sock.accept()
+                    with client_sock:
 
-                    # 操作情報をもとにモーターを制御
-                    command_type = receive_control[0]
-                    command_val = int(receive_control[1].rstrip(','))
+                        # クライアント情報表示
+                        print('GamepadController Client {')
+                        print(INDENT + 'IP   : {},'.format(client_addr[0]))
+                        print(INDENT + 'PORT : {}'.format(client_addr[1]))
+                        print('}')
 
-                    if command_type == 'ABS_Y':
-                        if command_val >= 0:
-                            motors_controller.change_motor_status(MotorsController.MotorSelection.LEFT_MOTOR, MotorsController.MotorRotationDirection.FORWARD, x_vector[0] * command_val / 4.0)
-                            motors_controller.change_motor_status(MotorsController.MotorSelection.RIGHT_MOTOR, MotorsController.MotorRotationDirection.FORWARD, x_vector[1] * command_val / 4.0)
-                        else:
-                            motors_controller.change_motor_status(MotorsController.MotorSelection.LEFT_MOTOR, MotorsController.MotorRotationDirection.BACKWARD, x_vector[0] * -command_val / 4.0)
-                            motors_controller.change_motor_status(MotorsController.MotorSelection.RIGHT_MOTOR, MotorsController.MotorRotationDirection.BACKWARD, x_vector[1] * -command_val / 4.0)
+                        while True:
+                            # 操作情報取得
+                            receive_control = client_sock.recv(2048).decode('utf-8').split(' ')
 
-                    if command_type == 'ABS_RX':
-                        if command_val >= 0:
-                            x_vector = [1.0, 1.0 - command_val / 4.0]
-                        else:
-                            x_vector = [1.0 + command_val / 4.0, 1.0]
+                            if not receive_control: break
+
+                            # 操作情報表示
+                            print('GamepadController Command {')
+                            print(INDENT + 'Type : {}'.format(receive_control[0]))
+                            print(INDENT + 'Value: {}'.format(receive_control[1]))
+                            print('}')
+
+                            # 複数のコマンドが連結されていた場合は操作を拒否
+                            if len(receive_control[1]) != receive_control[1].find(',') + 1:
+                                continue
+
+                            # 操作情報をもとにモーターを制御
+                            command_type = receive_control[0]
+                            command_val = int(receive_control[1].rstrip(','))
+
+                            if command_type == 'ABS_Y':
+                                if command_val >= 0:
+                                    motors_controller.change_motor_status(MotorsController.MotorSelection.LEFT_MOTOR, MotorsController.MotorRotationDirection.FORWARD, x_vector[0] * command_val / 4.0)
+                                    motors_controller.change_motor_status(MotorsController.MotorSelection.RIGHT_MOTOR, MotorsController.MotorRotationDirection.FORWARD, x_vector[1] * command_val / 4.0)
+                                else:
+                                    motors_controller.change_motor_status(MotorsController.MotorSelection.LEFT_MOTOR, MotorsController.MotorRotationDirection.BACKWARD, x_vector[0] * -command_val / 4.0)
+                                    motors_controller.change_motor_status(MotorsController.MotorSelection.RIGHT_MOTOR, MotorsController.MotorRotationDirection.BACKWARD, x_vector[1] * -command_val / 4.0)
+
+                            if command_type == 'ABS_RX':
+                                if command_val >= 0:
+                                    x_vector = [1.0, 1.0 - command_val / 4.0]
+                                else:
+                                    x_vector = [1.0 + command_val / 4.0, 1.0]
+            except:
+                pass
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('./settings.ini', 'UTF-8')
-    gamepad_controller = GamepadController(21, 20, 23, 24, 1000, utility.get_server_ip(), int(config.get('gamepad', 'port')))
+    gamepad_controller = GamepadController(21, 20, 23, 24, 1000, utility.get_server_ip(), config.get('gamepad', 'port'))
     gamepad_controller.start()
     gamepad_controller.join()
--- WebCameraController.py
+++ WebCameraController.py
@@ -9,16 +9,16 @@ import utility
 class WebCameraController(threading.Thread):
     def __init__(self, camera_id, camera_fps, camera_width, camera_height, server_ip, server_port, header_size, image_width, image_height, image_quality):
         print("WebCameraController __init__")
-        self.CAMERA_ID = camera_id
-        self.CAMERA_FPS = camera_fps
-        self.CAMERA_WIDTH = camera_width
-        self.CAMERA_HEIGHT = camera_height
+        self.CAMERA_ID = int(camera_id)
+        self.CAMERA_FPS = int(camera_fps)
+        self.CAMERA_WIDTH = int(camera_width)
+        self.CAMERA_HEIGHT = int(camera_height)
         self.SERVER_IP = server_ip
-        self.SERVER_PORT = server_port
-        self.HEADER_SIZE = header_size
-        self.IMAGE_WIDTH = image_width
-        self.IMAGE_HEIGHT = image_height
-        self.IMAGE_QUALITY = image_quality
+        self.SERVER_PORT = int(server_port)
+        self.HEADER_SIZE = int(header_size)
+        self.IMAGE_WIDTH = int(image_width)
+        self.IMAGE_HEIGHT = int(image_height)
+        self.IMAGE_QUALITY = int(image_quality)
         threading.Thread.__init__(self)
 
     def __del__(self):
@@ -36,63 +36,67 @@ class WebCameraController(threading.Thread):
         cam.set(cv2.CAP_PROP_FRAME_WIDTH, self.CAMERA_WIDTH)
         cam.set(cv2.CAP_PROP_FRAME_HEIGHT, self.CAMERA_HEIGHT)
 
-        # カメラ情報表示
-        print('WebCameraController Camera {')
-        print(INDENT + 'ID    : {},'.format(self.CAMERA_ID))
-        print(INDENT + 'FORMAT: {},'.format(cam.get(cv2.CAP_PROP_FOURCC)))
-        print(INDENT + 'FPS   : {},'.format(cam.get(cv2.CAP_PROP_FPS)))
-        print(INDENT + 'WIDTH : {},'.format(cam.get(cv2.CAP_PROP_FRAME_WIDTH)))
-        print(INDENT + 'HEIGHT: {}'.format(cam.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-        print('}')
-
-        # サーバ情報表示
-        print('WebCameraController Server {')
-        print(INDENT + 'IP   : {},'.format(self.SERVER_IP))
-        print(INDENT + 'PORT : {}'.format(self.SERVER_PORT))
-        print('}')
-
-        # サーバーソケット生成
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
-            server_sock.bind((self.SERVER_IP, self.SERVER_PORT))
-            server_sock.listen(1)
-
-            # クライアント接続
-            client_sock, client_addr = server_sock.accept()
-            with client_sock:
-
-                # クライアント情報表示
-                print('WebCameraController Client {')
-                print(INDENT + 'IP   : {},'.format(client_addr[0]))
-                print(INDENT + 'PORT : {}'.format(client_addr[1]))
+        while True:
+            try:
+                # カメラ情報表示
+                print('WebCameraController Camera {')
+                print(INDENT + 'ID    : {},'.format(self.CAMERA_ID))
+                print(INDENT + 'FORMAT: {},'.format(cam.get(cv2.CAP_PROP_FOURCC)))
+                print(INDENT + 'FPS   : {},'.format(cam.get(cv2.CAP_PROP_FPS)))
+                print(INDENT + 'WIDTH : {},'.format(cam.get(cv2.CAP_PROP_FRAME_WIDTH)))
+                print(INDENT + 'HEIGHT: {}'.format(cam.get(cv2.CAP_PROP_FRAME_HEIGHT)))
                 print('}')
 
-                # メインループ
-                while True:
-                    loop_start_time = time.time()
+                # サーバ情報表示
+                print('WebCameraController Server {')
+                print(INDENT + 'IP   : {},'.format(self.SERVER_IP))
+                print(INDENT + 'PORT : {}'.format(self.SERVER_PORT))
+                print('}')
 
-                    # 送信用画像データ作成
-                    flag, img = cam.read()
-                    resized_img = cv2.resize(img, (self.IMAGE_WIDTH, self.IMAGE_HEIGHT))
-                    (status, encoded_img) = cv2.imencode('.jpg', resized_img, [int(cv2.IMWRITE_JPEG_QUALITY), self.IMAGE_QUALITY])
+                # サーバーソケット生成
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_sock:
+                    server_sock.bind((self.SERVER_IP, self.SERVER_PORT))
+                    server_sock.listen(1)
 
-                    # パケット構築
-                    packet_body = encoded_img.tostring()
-                    packet_header = len(packet_body).to_bytes(self.HEADER_SIZE, 'big') 
-                    packet = packet_header + packet_body
+                    # クライアント接続
+                    client_sock, client_addr = server_sock.accept()
+                    with client_sock:
 
-                    # パケット送信
-                    try:
-                        client_sock.sendall(packet)
-                    except OSError as e:
-                        print('WebCameraController connection closed.')
-                        break
+                        # クライアント情報表示
+                        print('WebCameraController Client {')
+                        print(INDENT + 'IP   : {},'.format(client_addr[0]))
+                        print(INDENT + 'PORT : {}'.format(client_addr[1]))
+                        print('}')
 
-                    # FPS制御
-                    time.sleep(max(0, 1 / FPS - (time.time() - loop_start_time)))
+                        # メインループ
+                        while True:
+                            loop_start_time = time.time()
+
+                            # 送信用画像データ作成
+                            flag, img = cam.read()
+                            resized_img = cv2.resize(img, (self.IMAGE_WIDTH, self.IMAGE_HEIGHT))
+                            (status, encoded_img) = cv2.imencode('.jpg', resized_img, [int(cv2.IMWRITE_JPEG_QUALITY), self.IMAGE_QUALITY])
+
+                            # パケット構築
+                            packet_body = encoded_img.tostring()
+                            packet_header = len(packet_body).to_bytes(self.HEADER_SIZE, 'big') 
+                            packet = packet_header + packet_body
+
+                            # パケット送信
+                            try:
+                                client_sock.sendall(packet)
+                            except OSError as e:
+                                print('WebCameraController connection closed.')
+                                break
+
+                            # FPS制御
+                            time.sleep(max(0, 1 / FPS - (time.time() - loop_start_time)))
+            except:
+                pass
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('./settings.ini', 'UTF-8')
-    web_camera_controller = WebCameraController(0, 30, 1280, 720, utility.get_server_ip(), int(config.get('web_camera', 'port')), int(config.get('web_camera', 'header_size')), int(config.get('web_camera', 'image_width')), int(config.get('web_camera', 'image_height')), 30)
+    web_camera_controller = WebCameraController(0, 30, 1280, 720, utility.get_server_ip(), config.get('web_camera', 'port'), config.get('web_camera', 'header_size'), config.get('web_camera', 'image_width'), config.get('web_camera', 'image_height'), 30)
     web_camera_controller.start()
     web_camera_controller.join()
